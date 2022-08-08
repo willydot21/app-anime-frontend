181aabaa8fdf3afc35ff18f00a558c2ee17f267d
willydot21 <willyusername.getall@gmail.com>
2022-08-08 18:17:39 +0200

    fixing styles and btn navigation class

--- src/components/navigation/navigation-bar/navigation-bar.tsx
+++ src/components/navigation/navigation-bar/navigation-bar.tsx
@@ -2,21 +2,24 @@
 import './navigation-bar.css';
 import { v4 as uuid } from 'uuid';
 
-const NavigationBar = ({ sections, navigationBarRef, click }: {
+const NavigationBar = ({ sections, navigationBarRef, click, active }: {
   sections: string[],
   navigationBarRef: React.RefObject<HTMLDivElement>,
-  click: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  click: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
+  active: string
 }) => {
-
   return (
     <div className="navigation-bar" ref={navigationBarRef} >
       <span className="navigation-bar-label" />
       {
-        sections.map(section => (
-          <button className="navigation-bar-button" onClick={click} name={section} key={uuid()}>
-            {section}
-          </button>
-        ))
+        sections.map(section => {
+          const activeClassButton = active === section ? 'navigation-bar-active' : ''
+          return (
+            <button className={"navigation-bar-button " + activeClassButton} onClick={click} name={section} key={uuid()}>
+              {section}
+            </button>
+          );
+        })
       }
     </div>
   );
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -5,48 +5,49 @@ import { NavigationProps } from './navigation-types';
 import NavigationBar from "./navigation-bar/navigation-bar";
 import { changeSection, setActiveSection, setupOnChange } from "./navigation.utils";
 
-const Navigation = ({ elements, defaultItem }:NavigationProps) => {
+const Navigation = ({ elements, defaultItem }: NavigationProps) => {
 
-  const [active, setActive] = useState( defaultItem || Object.keys(elements)[0] );
+  const [active, setActive] = useState(defaultItem || Object.keys(elements)[0]);
 
   const navigationBar = useRef<HTMLDivElement>(null);
 
   const container = useRef<HTMLDivElement>(null);
 
-  const handlerOnClick = function(e:React.MouseEvent<HTMLButtonElement, MouseEvent>){ 
-    changeSection( e.target as HTMLInputElement, setActive, navigationBar ); 
+  const handlerOnClick = function (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    changeSection(e.target as HTMLInputElement, setActive, navigationBar);
   }
   // handler to call changeSection onClick.
 
-  useEffect( () => {
+  useEffect(() => {
 
-    setupOnChange( setActive, active, navigationBar );
+    setupOnChange(setActive, active, navigationBar);
     // when 'active' state change, call setup function and
     // and set window.onresize event, but with new active value.
 
-    setActiveSection( container, active );
+    setActiveSection(container, active);
     // set active component and hidde inactive, so 
     // you don't have to reload again.
 
   }, [active]);
 
-  useEffect( () => {
+  useEffect(() => {
 
-    setupOnChange( setActive, active, navigationBar, true );
-    
+    setupOnChange(setActive, active, navigationBar, true);
+
   }, []); // on load.
 
   return (
     <div className="navigation">
 
-      <NavigationBar 
-        sections={ Object.keys(elements) }
-        navigationBarRef={ navigationBar }
-        click={ handlerOnClick }
+      <NavigationBar
+        active={active}
+        sections={Object.keys(elements)}
+        navigationBarRef={navigationBar}
+        click={handlerOnClick}
       />
 
-      <div className="navigation-container" ref={ container } >
-        { Object.values(elements) }
+      <div className="navigation-container" ref={container} >
+        {Object.values(elements)}
       </div>
 
     </div>
